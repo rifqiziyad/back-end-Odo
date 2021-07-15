22c7e9f72e2df74d32bf72876946dfce6a88cd55
rifqiziyad <rifqiimtinan@gmail.com>
2021-07-15 22:22:58 +0700

    update end point create transaction

--- src/modules/Transaction/transaction_controller.js
+++ src/modules/Transaction/transaction_controller.js
@@ -8,12 +8,8 @@ module.exports = {
   createTransaction: async (req, res) => {
     try {
       const { transactionSenderId, transactionReceiverId } = req.query
-      const {
-        transactionAmount,
-        transactionStatus,
-        transactionMessage,
-        userPin
-      } = req.body
+      const { transactionAmount, transactionStatus, transactionMessage } =
+        req.body
       const setData = {
         transaction_sender_id: transactionSenderId,
         transaction_receiver_id: transactionReceiverId,
@@ -23,24 +19,9 @@ module.exports = {
         // eslint-disable-next-line eqeqeq
         transaction_type: transactionSenderId == 0 ? 'Top Up' : 'Transfer'
       }
-      const checkIdUser = await transactionModel.getUserDataById({
-        user_id: transactionSenderId
-      })
 
-      if (checkIdUser.length > 0) {
-        if (checkIdUser[0].user_pin === userPin) {
-          const result = await transactionModel.createData(setData)
-          return helper.response(res, 200, 'Success Transaction', result)
-        } else {
-          return helper.response(res, 405, 'Wrong Pin')
-        }
-      } else {
-        return helper.response(
-          res,
-          404,
-          `Data User By Id: ${transactionSenderId} Not Found,`
-        )
-      }
+      const result = await transactionModel.createData(setData)
+      return helper.response(res, 200, 'Success Transaction', result)
     } catch (error) {
       return helper.response(res, 400, 'Bad Request', error)
     }
